6e56cb5cb417f8ce36af2147d29598bec8fe9ada
Mia <mia-dl@tuta.io>
2025-05-19 12:58:18 +0200

    edits

--- link-list.sh
+++ link-list.sh
@@ -1,7 +1,7 @@
 counter=1
 for i in ./*; do
   i="${i#./}"
-  if [ "$i" != "index.html" ] && [ "$i" != "style.css" ] && [ "$i" != "favicon.png" ] && [ "$i" != "gen.sh" ] && [ "$i" != "readme.sh" ] && [ "$i" != "link-list.sh" ]; then
+  if [ "$i" != "LICENSE" ] && [ "$i" != "link-list" ] && [ "$i" != "custom-http" ] && [ "$i" != "README" ]  && [ "$i" != "index.html" ] && [ "$i" != "style.css" ] && [ "$i" != "favicon.png" ] && [ "$i" != "gen.sh" ] && [ "$i" != "readme.sh" ] && [ "$i" != "link-list.sh" ]; then
     printf '%s:./%s\n' "$counter" "${i#./}"
     : $((counter+=1))
   fi
--- readme.sh
+++ readme.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 counter=1
-printf '%s\nwrititngs\n%s\n' "---" "---"
+printf '%s\nwritings\n%s\n' "---" "---"
 for i in ./*; do
   i="${i#./}"
-  if [ "$i" != "index.html" ] && [ "$i" != "style.css" ] && [ "$i" != "favicon.png" ] && [ "$i" != "gen.sh" ] && [ "$i" != "readme.sh" ] && [ "$i" != "link-list.sh" ]; then
+  if [ "$i" != "LICENSE" ] && [ "$i" != "link-list" ] && [ "$i" != "custom-http" ] && [ "$i" != "README" ]  && [ "$i" != "index.html" ] && [ "$i" != "style.css" ] && [ "$i" != "favicon.png" ] && [ "$i" != "gen.sh" ] && [ "$i" != "readme.sh" ] && [ "$i" != "link-list.sh" ]; then
     i="${i%.jpg}"; i="${i%.png}"
     printf '://%s\n' "${i#./}"
     : $((counter+=1))
